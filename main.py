7cd8318ad576db543604cabd7f64038be166d613
Tyrant <tyler.sauve@bag-of-holding.com>
2025-02-05 20:41:35 -0800

    Data

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import torch
+from torch.utils.data import (DataLoader, Dataset)
+
+class ChessDataset(Dataset):
+    def __init__(self, file_path):
+        self.file_path = file_path
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
 
 
 if __name__ == '__main__':
-    print("Hello, world")
+    print(torch.__version__)
 
